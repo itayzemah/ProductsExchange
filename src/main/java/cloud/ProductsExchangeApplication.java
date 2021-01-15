e27eb68fc8cc440e87ced6d155b8c6d1c8c80e78
ItayZemah <itayzemah@users.noreply.github.com>
2021-01-15 18:31:55 +0200

    first commit

---
+++ src/main/java/cloud/ProductsExchangeApplication.java
@@ -0,0 +1,13 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsExchangeApplication.class, args);
+	}
+
+}
