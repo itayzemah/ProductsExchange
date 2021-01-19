e016d5a567c2bb9e0ae6fe5f82176c1068741c7d
Itay Zemah <itayzemah@gmail.com>
2021-01-19 19:09:41 +0200

    added consumer

--- src/main/java/cloud/logic/ExchangeServiceImplementation.java
+++ src/main/java/cloud/logic/ExchangeServiceImplementation.java
@@ -15,20 +15,25 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ExchangeServiceImplementation implements ExchangeService {
-
+	private ProductConsumer productConsumer;
 	private ExchangeDataAccessRepository exchangeDAL;
 	private ExchangeConverted converter;
 
 	@Override
 	public ExchangeBoundary[] getAll(int page, int size) {
-		return exchangeDAL.findAll(PageRequest.of(page, size)).stream().map(converter::toBoundary)
+		return exchangeDAL.findAll(PageRequest.of(page, size))
+				.stream()
+				.map(converter::toBoundary)
+				.map(productConsumer::setProduct)
 				.collect(Collectors.toList()).toArray(new ExchangeBoundary[0]);
 	}
 
 	@Override
 	public ExchangeBoundary getBidById(String bid) {
-		return this.converter.toBoundary(exchangeDAL.findById(bid)
+		ExchangeBoundary withoutProductDetails =  this.converter.toBoundary(exchangeDAL.findById(bid)
 				.orElseThrow(() -> new BidNotFoundException("A bid with id: " + bid + " not found")));
+		ExchangeBoundary withProduct = this.productConsumer.setProduct(withoutProductDetails);
+		return withProduct;
 	}
 
 	@Override
---
+++ src/main/java/cloud/logic/ProductConsumer.java
@@ -0,0 +1,8 @@
+package cloud.logic;
+
+import cloud.boundaries.ExchangeBoundary;
+
+public interface ProductConsumer {
+
+	public ExchangeBoundary setProduct(ExchangeBoundary bid);
+}
