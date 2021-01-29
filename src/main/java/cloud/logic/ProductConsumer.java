46c7eca963f239dda9604ac067a00c1af1da4116
Shachar Nissan <Shachar208@gmail.com>
2021-01-29 13:16:21 +0200

    isProductExist consumer

--- src/main/java/cloud/logic/ProductConsumer.java
+++ src/main/java/cloud/logic/ProductConsumer.java
@@ -1,10 +1,9 @@
 package cloud.logic;
 
 import cloud.boundaries.ExchangeBoundary;
-import cloud.boundaries.ProductBoundary;
 
 public interface ProductConsumer {
 
 	public ExchangeBoundary setProduct(ExchangeBoundary bid);
-	public ProductBoundary createProduct(ProductBoundary product);
+	public boolean isProductExist(String productId);
 }
